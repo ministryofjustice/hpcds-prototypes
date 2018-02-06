a5162694f1903532c7328c0fa0322b39d599cdad
Nick Rowlands <nick@rowlando.com>
2018-02-06 16:36:41 +0000

    Prototype version 6 screens

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,18 @@
-var express = require('express')
-var router = express.Router()
+var express = require("express");
+var router = express.Router();
 
 // Route index page
-router.get('/', function (req, res) {
-  res.render('index.html')
-})
+router.get("/", function(req, res) {
+  res.render("index.html");
+});
 
 // add your routes here
 
-module.exports = router
+router.post("/v6/prepare", function(req, res) {
+  if (req.body.attendhpcds === "no") {
+    res.redirect("/v6/no-claims");
+  }
+  res.redirect("/v6/prepare");
+});
+
+module.exports = router;
